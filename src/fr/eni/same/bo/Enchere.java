dff6cb711784d78019556978d64eb7be31fedb9c
sl <s.leyral@gmail.com>
2020-05-11 15:15:07 +0200

    Bo utilisateurs
    Mise en place des constructeurs

--- src/fr/eni/same/bo/Enchere.java
+++ src/fr/eni/same/bo/Enchere.java
@@ -6,5 +6,29 @@ public class Enchere {
 	private LocalDate dateEnchère;
 	private Utilisateur utilisateurEnchere;
 	private Vente venteEnchere;
+	
+	
+	public LocalDate getDateEnchère() {
+		return dateEnchère;
+	}
+	public void setDateEnchère(LocalDate dateEnchère) {
+		this.dateEnchère = dateEnchère;
+	}
+	public Utilisateur getUtilisateurEnchere() {
+		return utilisateurEnchere;
+	}
+	public void setUtilisateurEnchere(Utilisateur utilisateurEnchere) {
+		this.utilisateurEnchere = utilisateurEnchere;
+	}
+	public Vente getVenteEnchere() {
+		return venteEnchere;
+	}
+	public void setVenteEnchere(Vente venteEnchere) {
+		this.venteEnchere = venteEnchere;
+	}
+	
+	
+	
+	
 }
 
