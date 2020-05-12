7b2647ae11a81a05ffa935047453a38b705bb0cd
Etienne <etienne.fdarcier@hotmail.fr>
2020-05-12 13:55:06 +0200

    Servlet de connection test
    Fichier créé à des fins de test avec la base de donnée. A utiliser seulement pour tester votre base

---
+++ src/fr/eni/same/dal/ServletConnectionTestEtienne.java
@@ -0,0 +1,47 @@
+package fr.eni.same.dal;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConnectionTestServlet
+ */
+@WebServlet("/ServletConnectionTestEtienne")
+public class ServletConnectionTestEtienne extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection con = null;
+		String query = "SELECT * FROM categories";
+		con = ConnectionProvider.openConnection();
+		try {
+			PreparedStatement stmt = con.prepareStatement(query);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				System.out.println(rs.getString(1));				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		con = ConnectionProvider.closeConnection();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
